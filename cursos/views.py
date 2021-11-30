6b4a76d2fb6a50ace48e62ba036500c2e7d14a2c
johnpligeiro <johnpligeiro@gmail.com>
2021-11-30 20:25:48 -0300

    api v2 using viewsets.ModelViewSet

--- cursos/views.py
+++ cursos/views.py
@@ -1,10 +1,12 @@
-from rest_framework import generics
+from rest_framework import generics, viewsets
 from rest_framework.generics import get_object_or_404
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 from .models import Curso, Avaliacao
 from .serializers import CursoSerializer, AvaliacaoSerializer
 
-
+#v1
 class CursosAPIView(generics.ListCreateAPIView):
     queryset = Curso.objects.all()
     serializer_class = CursoSerializer
@@ -35,3 +37,20 @@ class AvaliacaoAPIView(generics.RetrieveUpdateDestroyAPIView):
                                      curso_id=self.kwargs.get('curso_pk'),
                                      pk=self.kwargs.get('avaliacao_pk'))
         return get_object_or_404(self.get_queryset(), pk=self.kwargs.get('avaliacao_pk'))
+
+#v2
+
+class CursoViewSet(viewsets.ModelViewSet):
+    queryset = Curso.objects.all()
+    serializer_class = CursoSerializer
+
+    @action(detail=True, methods=['get'])
+    def avaliacoes(self, request, pk=None):
+        curso = self.get_object()
+        serializer = AvaliacaoSerializer(curso.avaliacoes.all(), many=True)
+        return Response(serializer.data)
+
+
+class AvaliacaoViewSet(viewsets.ModelViewSet):
+    queryset = Avaliacao.objects.all()
+    serializer_class = AvaliacaoSerializer
