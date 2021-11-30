7269a69aa889a75ddeb1b3547baa5b20b4348d61
johnpligeiro <johnpligeiro@gmail.com>
2021-11-30 13:46:02 -0300

    configure settings, installed apps, DRF auth and auth url

--- escola/settings.py
+++ escola/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_filters',
+    'rest_framework',
     'cursos',
 ]
 
@@ -127,3 +129,14 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# DRF
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.SessionAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticateOrReadOnly'
+    )
+}
